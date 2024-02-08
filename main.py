5b04464a6036a95e1d4de49dcb4c11baf2f95a52
Abigail <abigail.leland@students.fhu.edu>
2024-02-08 12:19:33 -0600

    main.py
    github check

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    length = len(word)
+    if (length % 2) == 0:
+        half_length = int(length / 2)
+    else:
+        half_length = int((length - 1) / 2)
+    helpful_length = half_length * 2
+    for i in range(helpful_length):
+        if word[]
+
+    
 
 if __name__ == '__main__': 
     #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    user_input = input()
+    print(palindrome(user_input))
