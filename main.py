b870ac2be7fe7171c88370512385655f75a12946
Abigail <abigail.leland@students.fhu.edu>
2024-02-09 11:19:10 -0600

    main.py
    first real draft

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ def palindrome(word):
         half_length = int(length / 2)
     else:
         half_length = int((length - 1) / 2)
-    helpful_length = half_length * 2
-    for i in range(helpful_length):
-        if word[]
+    front_index = 0
+    back_index = length - 1
+    for i in range(half_length):
+        if word[front_index] == word[back_index]:
+            continue
+        else:
+            return False
+        front_index += 1
+        back_index -= 1
+    return True
+        
 
     
 
